a6973eb739d9cddf2b95630dd4d937d41c91295b
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-03-30 23:41:42 +0700

    Implement calculation of the area and perimeter of the figures

---
+++ area_and_perimeter_calculation.h
@@ -0,0 +1,11 @@
+#ifndef _AREA_AND_PERIMETER_CALCULATION_H
+#define _AREA_AND_PERIMETER_CALCULATION_H
+#include <cmath>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+double perimeter(string s);
+double area(string s);
+#endif
