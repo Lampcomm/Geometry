f815ac19d5f6b2ed2b04e5fc2c549d0a547c29cc
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-04-23 23:24:53 +0700

    Generate file test.cpp for program testing

---
+++ test/test.cpp
@@ -0,0 +1,227 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#define _CXXTEST_HAVE_STD
+#define _CXXTEST_HAVE_EH
+#define _CXXTEST_ABORT_TEST_ON_FAIL
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/TestMain.h>
+#include <cxxtest/ErrorPrinter.h>
+
+int main( int argc, char *argv[] ) {
+ int status;
+    CxxTest::ErrorPrinter tmp;
+    CxxTest::RealWorldDescription::_worldName = "cxxtest";
+    status = CxxTest::Main< CxxTest::ErrorPrinter >( tmp, argc, argv );
+    return status;
+}
+bool suite_input_test_init = false;
+#include "input_test.h"
+
+static input_test suite_input_test;
+
+static CxxTest::List Tests_input_test = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_input_test( "input_test.h", 5, "input_test", suite_input_test, Tests_input_test );
+
+static class TestDescription_suite_input_test_test_input_circle1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_circle1() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 7, "test_input_circle1" ) {}
+ void runTest() { suite_input_test.test_input_circle1(); }
+} testDescription_suite_input_test_test_input_circle1;
+
+static class TestDescription_suite_input_test_test_input_circle2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_circle2() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 11, "test_input_circle2" ) {}
+ void runTest() { suite_input_test.test_input_circle2(); }
+} testDescription_suite_input_test_test_input_circle2;
+
+static class TestDescription_suite_input_test_test_input_circle3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_circle3() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 16, "test_input_circle3" ) {}
+ void runTest() { suite_input_test.test_input_circle3(); }
+} testDescription_suite_input_test_test_input_circle3;
+
+static class TestDescription_suite_input_test_test_input_circle4 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_circle4() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 21, "test_input_circle4" ) {}
+ void runTest() { suite_input_test.test_input_circle4(); }
+} testDescription_suite_input_test_test_input_circle4;
+
+static class TestDescription_suite_input_test_test_input_circle5 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_circle5() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 26, "test_input_circle5" ) {}
+ void runTest() { suite_input_test.test_input_circle5(); }
+} testDescription_suite_input_test_test_input_circle5;
+
+static class TestDescription_suite_input_test_test_input_triangle1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_triangle1() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 31, "test_input_triangle1" ) {}
+ void runTest() { suite_input_test.test_input_triangle1(); }
+} testDescription_suite_input_test_test_input_triangle1;
+
+static class TestDescription_suite_input_test_test_input_triangle2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_triangle2() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 35, "test_input_triangle2" ) {}
+ void runTest() { suite_input_test.test_input_triangle2(); }
+} testDescription_suite_input_test_test_input_triangle2;
+
+static class TestDescription_suite_input_test_test_input_triangle3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_triangle3() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 40, "test_input_triangle3" ) {}
+ void runTest() { suite_input_test.test_input_triangle3(); }
+} testDescription_suite_input_test_test_input_triangle3;
+
+static class TestDescription_suite_input_test_test_input_triangle4 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_triangle4() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 44, "test_input_triangle4" ) {}
+ void runTest() { suite_input_test.test_input_triangle4(); }
+} testDescription_suite_input_test_test_input_triangle4;
+
+static class TestDescription_suite_input_test_test_input_triangle5 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_triangle5() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 49, "test_input_triangle5" ) {}
+ void runTest() { suite_input_test.test_input_triangle5(); }
+} testDescription_suite_input_test_test_input_triangle5;
+
+static class TestDescription_suite_input_test_test_input_triangle6 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_input_test_test_input_triangle6() : CxxTest::RealTestDescription( Tests_input_test, suiteDescription_input_test, 53, "test_input_triangle6" ) {}
+ void runTest() { suite_input_test.test_input_triangle6(); }
+} testDescription_suite_input_test_test_input_triangle6;
+
+#include "intersection_test.h"
+
+static intesection_test suite_intesection_test;
+
+static CxxTest::List Tests_intesection_test = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_intesection_test( "intersection_test.h", 5, "intesection_test", suite_intesection_test, Tests_intesection_test );
+
+static class TestDescription_suite_intesection_test_test_intersection_circles1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_circles1() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 7, "test_intersection_circles1" ) {}
+ void runTest() { suite_intesection_test.test_intersection_circles1(); }
+} testDescription_suite_intesection_test_test_intersection_circles1;
+
+static class TestDescription_suite_intesection_test_test_intersection_circles2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_circles2() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 12, "test_intersection_circles2" ) {}
+ void runTest() { suite_intesection_test.test_intersection_circles2(); }
+} testDescription_suite_intesection_test_test_intersection_circles2;
+
+static class TestDescription_suite_intesection_test_test_intersection_circles3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_circles3() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 16, "test_intersection_circles3" ) {}
+ void runTest() { suite_intesection_test.test_intersection_circles3(); }
+} testDescription_suite_intesection_test_test_intersection_circles3;
+
+static class TestDescription_suite_intesection_test_test_intersection_triangles1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_triangles1() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 20, "test_intersection_triangles1" ) {}
+ void runTest() { suite_intesection_test.test_intersection_triangles1(); }
+} testDescription_suite_intesection_test_test_intersection_triangles1;
+
+static class TestDescription_suite_intesection_test_test_intersection_triangles2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_triangles2() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 28, "test_intersection_triangles2" ) {}
+ void runTest() { suite_intesection_test.test_intersection_triangles2(); }
+} testDescription_suite_intesection_test_test_intersection_triangles2;
+
+static class TestDescription_suite_intesection_test_test_intersection_triangles3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_triangles3() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 36, "test_intersection_triangles3" ) {}
+ void runTest() { suite_intesection_test.test_intersection_triangles3(); }
+} testDescription_suite_intesection_test_test_intersection_triangles3;
+
+static class TestDescription_suite_intesection_test_test_intersection_triangle_and_circle1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_triangle_and_circle1() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 44, "test_intersection_triangle_and_circle1" ) {}
+ void runTest() { suite_intesection_test.test_intersection_triangle_and_circle1(); }
+} testDescription_suite_intesection_test_test_intersection_triangle_and_circle1;
+
+static class TestDescription_suite_intesection_test_test_intersection_triangle_and_circle2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_triangle_and_circle2() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 51, "test_intersection_triangle_and_circle2" ) {}
+ void runTest() { suite_intesection_test.test_intersection_triangle_and_circle2(); }
+} testDescription_suite_intesection_test_test_intersection_triangle_and_circle2;
+
+static class TestDescription_suite_intesection_test_test_intersection_triangle_and_circle3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_intesection_test_test_intersection_triangle_and_circle3() : CxxTest::RealTestDescription( Tests_intesection_test, suiteDescription_intesection_test, 59, "test_intersection_triangle_and_circle3" ) {}
+ void runTest() { suite_intesection_test.test_intersection_triangle_and_circle3(); }
+} testDescription_suite_intesection_test_test_intersection_triangle_and_circle3;
+
+#include "area_and_perimeter_calculation_test.h"
+
+static area_and_perimeter_calculation_test suite_area_and_perimeter_calculation_test;
+
+static CxxTest::List Tests_area_and_perimeter_calculation_test = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_area_and_perimeter_calculation_test( "area_and_perimeter_calculation_test.h", 5, "area_and_perimeter_calculation_test", suite_area_and_perimeter_calculation_test, Tests_area_and_perimeter_calculation_test );
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation1() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 7, "test_perimeter_calculation1" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_perimeter_calculation1(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation1;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation2() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 11, "test_perimeter_calculation2" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_perimeter_calculation2(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation2;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation3() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 15, "test_perimeter_calculation3" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_perimeter_calculation3(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation3;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation4 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation4() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 20, "test_perimeter_calculation4" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_perimeter_calculation4(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation4;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation5 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation5() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 27, "test_perimeter_calculation5" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_perimeter_calculation5(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_perimeter_calculation5;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation1() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 31, "test_area_calculation1" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_area_calculation1(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_area_calculation1;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation2() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 36, "test_area_calculation2" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_area_calculation2(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_area_calculation2;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation3() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 40, "test_area_calculation3" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_area_calculation3(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_area_calculation3;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation4 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation4() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 44, "test_area_calculation4" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_area_calculation4(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_area_calculation4;
+
+static class TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation5 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_area_and_perimeter_calculation_test_test_area_calculation5() : CxxTest::RealTestDescription( Tests_area_and_perimeter_calculation_test, suiteDescription_area_and_perimeter_calculation_test, 49, "test_area_calculation5" ) {}
+ void runTest() { suite_area_and_perimeter_calculation_test.test_area_calculation5(); }
+} testDescription_suite_area_and_perimeter_calculation_test_test_area_calculation5;
+
+#include <cxxtest/Root.cpp>
+const char* CxxTest::RealWorldDescription::_worldName = "cxxtest";
